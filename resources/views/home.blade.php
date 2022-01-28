f064b4efb253815da2a7cb790d2a21d4f301561c
“Cal-107” <kaly___7@hotmail.it>
2022-01-28 15:01:49 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+   <h1 class="h-100 d-flex justify-content-center align-items-center">
+       Welcome to the Jokes Site
+   </h1>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light p-3 text-center">
+    The Best Jokes Site Ever© is protected by copyright, with &hearts;
+</footer>
